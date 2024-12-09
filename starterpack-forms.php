3b6c395e5b97a81316d77905f0639ee6c66c3017
Charlotte Habre <charlotte@superhuit.ch>
2024-12-09 14:18:48 +0100

    v1.1.0 ✨

--- starterpack-forms.php
+++ starterpack-forms.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Plugin Name:       Starterpack Forms
  * Plugin URI:        https://github.com/superhuit-agency/starterpack-forms
  * Description:       Starterpack's Form Custom Post Type.
- * Version:           1.0.17
+ * Version:           1.1.0
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            superhuit <tech@superhuit.ch>
@@ -24,24 +25,24 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Starterpack i18n. If not, see {URI to Plugin License}.
-*/
+ */
 
-defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+defined('ABSPATH') or die('Cheatin&#8217; uh?');
 
-define( 'SPCKFORMS_PLUGIN_VERSION', '1.0.17' );
-define( 'SPCKFORMS_PATH', __DIR__ );
-define( 'SPCKFORMS_URI', plugin_dir_url(__FILE__) );
+define('SPCKFORMS_PLUGIN_VERSION', '1.1.0');
+define('SPCKFORMS_PATH', __DIR__);
+define('SPCKFORMS_URI', plugin_dir_url(__FILE__));
 
 // Load dependencies
 // ====
-if ( ! file_exists(__DIR__ . '/vendor/autoload.php') ) {
-	add_action( 'admin_notices', function() {
-		?>
+if (! file_exists(__DIR__ . '/vendor/autoload.php')) {
+	add_action('admin_notices', function () {
+?>
 		<div class="notice notice-warning">
-			<p><?php _e( 'Please install composer dependencies for Starterpack Form CPT to work', 'spckforms' ); ?></p>
+			<p><?php _e('Please install composer dependencies for Starterpack Form CPT to work', 'spckforms'); ?></p>
 		</div>
-		<?php
-	} );
+<?php
+	});
 	return;
 }
 
@@ -56,32 +57,35 @@ require_once __DIR__ . '/submission.php';
 // ====
 // Action & filter hooks
 // ====
-register_activation_hook( __FILE__, 'spckforms_activate' );
-register_deactivation_hook( __FILE__, 'spckforms_deactivate' );
+register_activation_hook(__FILE__, 'spckforms_activate');
+register_deactivation_hook(__FILE__, 'spckforms_deactivate');
 register_uninstall_hook(__FILE__, 'spckforms_uninstall');
 
 // Load translation texts
-add_action( 'after_setup_theme', function() {
-	load_theme_textdomain( 'spckforms', SPCKFORMS_PATH . '/languages' );
+add_action('after_setup_theme', function () {
+	load_theme_textdomain('spckforms', SPCKFORMS_PATH . '/languages');
 });
 
 /**
  * Execute anything necessary on plugin activation
  */
-function spckforms_activate() {
+function spckforms_activate()
+{
 	// e.g. Save default options to database
 }
 
 /**
  * Execute anything necessary on plugin deactivation
  */
-function spckforms_deactivate() {
+function spckforms_deactivate()
+{
 	// e.g. delete cache or temp options
 }
 
 /**
  * Execute anything necessary on plugin uninstall (deletion)
  */
-function spckforms_uninstall() {
+function spckforms_uninstall()
+{
 	// e.g. remove plugin options from database
 }
